2ef3ff605a1fa90610ac365041aa1cea36e52bcb
guzhegnyang <53924636+guzhegnyang@users.noreply.github.com>
2020-01-09 21:47:29 +0800

    Update 5-2.cpp

--- 5-2.cpp
+++ 5-2.cpp
@@ -7,18 +7,19 @@ using namespace std;
 int n, m;
 int main() {
     scanf("%d %d", &n, &m);
-    int t = 1;
     queue<int> q1, q2;
-    vector<char> s, a;
+    vector<short> s;
+    queue<int> bucket[10];
+    int t = 1, len = 0;
     while (n--) {
+        printf("%d", t);
         q1.push(2 * t + 1);
         q2.push(4 * t + 5);
         for (; t; t /= 10) {
-            s.push_back(t % 10 + '0');
+            s.push_back(t % 10);
         }
         for (; !s.empty(); s.pop_back()) {
-            putchar(s.back());
-            a.push_back(s.back());
+            bucket[s.back()].push(len++);
         }
         if (q1.front() < q2.front()) {
             t = q1.front();
@@ -30,13 +31,15 @@ int main() {
         }
     }
     putchar('\n');
-    int l, r;
-    for (l = 0, r = m; r < a.size(); l++, r++) {
-        for (t = l; t <= r; t++) {
-            if (a[t] > a[l]) {
-                l = t;
+    int i;
+    for (i = 0; i < len - m; i++) {
+        for (t = 9; t >= 0; t--) {
+            for (; !bucket[t].empty() && bucket[t].front() <= n; bucket[t].pop());
+            if (!bucket[t].empty() && bucket[t].front() <= m + i) {
+                putchar(t + '0');
+                n = bucket[t].front();
+                break;
             }
         }
-        putchar(a[l]);
     }
 }

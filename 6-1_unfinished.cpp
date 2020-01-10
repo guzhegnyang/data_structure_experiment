5226c8009a1e25ea4dae133424d5735cc72ae8c4
guzhegnyang <53924636+guzhegnyang@users.noreply.github.com>
2020-01-10 22:51:54 +0800

    Update 6-1_unfinished.cpp

--- 6-1_unfinished.cpp
+++ 6-1_unfinished.cpp
@@ -5,20 +5,20 @@
 using namespace std;
 string s;
 int main() {
-    cin >> s;
-    int i = 0, j = 0, vis[256], ans = 0;
-    memset(vis, -1, sizeof(vis));
-    vis[s[0]] = 0;
-    while (j < s.length()) {
-        if (vis[s[j + 1]] == -1) {
-            vis[s[j + 1]] = j + 1;
-        }
-        else {
-            ans = max(ans, j + 1 - i);
-            i = vis[s[j + 1]] + 1;
-            vis[s[j + 1]] = j + 1;
-        }
-        j++;
-    }
-    cout << ans;
+	cin >> s;
+	int i = 0, j = 0, vis[256], ans = 0;
+	memset(vis, -1, sizeof(vis));
+	vis[s[0]] = 0;
+	while (j < s.length()) {
+		ans = max(ans, j + 1 - i);
+		if (vis[s[j + 1]] == -1) {
+			vis[s[j + 1]] = j + 1;
+		    j++;
+		}
+		else {
+            vis[s[i]] = -1;
+            i++;
+		}
+	}
+	cout << ans;
 }

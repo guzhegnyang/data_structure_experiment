018ea00fd6539bb84df2a2052df1df30de1a7164
guzhegnyang <53924636+guzhegnyang@users.noreply.github.com>
2020-01-10 10:03:25 +0800

    Update 5-3_priority_queue_in_STL.cpp

--- 5-3_priority_queue_in_STL.cpp
+++ 5-3_priority_queue_in_STL.cpp
@@ -1,34 +1,34 @@
 #include<iostream>
-#include<vector>
 #include<queue>
 using namespace std;
-struct flight {
-	long long c;
+struct f {
 	long long n;
-	bool operator<(const flight& f) const;
+	long long w;
+	bool operator <(const f& ff) const {
+		return w < ff.w;
+	}
 };
-bool flight::operator<(const flight& f) const{
-	return c < f.c;
-}
+long long n, k;
 int main() {
-	long long n, k, i, cost = 0;
-	cin >> n >> k;
-	vector<flight> v(n, { -1, -1 });
-	vector<flight>::iterator iter;
-	for (i = 0; i < n; i++) {
-		cin >> v[i].c;
-		v[i].n = i;
-	}
-	for (i = 0, iter = v.begin(); i < k; i++, iter++);
-	priority_queue<flight> q(v.begin(), iter);
-	for (; i < n; i++) {
-		q.push(v[i]);
-		cost += (i - q.top().n) * q.top().c;
-		q.pop();
-	}
-	for (; i < n + k; i++) {
-		cost += (i - q.top().n) * q.top().c;
-		q.pop();
-	}
-	cout << cost;
+	scanf("%lld %lld", &n, &k);
+	long long i = 0, ans = 0;
+	priority_queue<f> pq;
+	f t;
+    for (; i < k; i++) {
+        scanf("%lld", &t.w);
+        t.n = i;
+        pq.push(t);
+    }
+    for (; i < n; i++) {
+        scanf("%lld", &t.w);
+        t.n = i;
+        pq.push(t);
+        ans += (i - pq.top().n) * pq.top().w;
+        pq.pop();
+    }
+    for (; i < n + k; i++) {
+        ans += (i - pq.top().n) * pq.top().w;
+        pq.pop();
+    }
+	printf("%lld", ans);
 }
